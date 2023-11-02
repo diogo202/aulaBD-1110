fe4efb3836bcc8a6b4687a97956a10bd8660dde1
diogo202 <65231191+diogo202@users.noreply.github.com>
2023-11-02 01:45:59 +0000

    scriptv2

--- script.sql
+++ script.sql
@@ -1,4 +1,22 @@
 -- comentario
 SHOW DATABASES;
 
+SHOW TABLES FROM banco_teste;
+
+SHOW FIELDS FROM banco_teste.alunos;
+
 CREATE DATABASE banco_teste;
+
+DROP DATABASE banco_teste;
+
+CREATE TABLE banco_teste.alunos(
+	`id` INT PRIMARy KEY,
+	`nome` VARCHAR(255)
+);
+
+USE banco_teste;
+
+CREATE TABLE teste (
+    id INT PRIMARY KEY,
+    valor CHAR(11)
+);
