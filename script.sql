761c976d353741361aaf82c79d80f27a8c71d6a7
diogo202 <65231191+diogo202@users.noreply.github.com>
2023-11-09 01:39:10 +0000

    V3

--- script.sql
+++ script.sql
@@ -1,17 +1,25 @@
 -- comentario
 SHOW DATABASES;
 
-SHOW TABLES FROM banco_teste;
+SHOW TABLES FROM biblioteca;
 
 SHOW FIELDS FROM banco_teste.alunos;
 
-CREATE DATABASE banco_teste;
+CREATE DATABASE biblioteca;
+
+USE biblioteca;
 
 DROP DATABASE banco_teste;
 
-CREATE TABLE banco_teste.alunos(
-	`id` INT PRIMARy KEY,
-	`nome` VARCHAR(255)
+CREATE TABLE generos(
+	`id` INT AUTO_INCREMENT PRIMARy KEY,
+	`nome` VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE livros(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    id_genero INT NOT NULL REFERENCES generos(id)
 );
 
 USE banco_teste;
@@ -20,3 +28,15 @@ CREATE TABLE teste (
     id INT PRIMARY KEY,
     valor CHAR(11)
 );
+
+INSERT INTO generos(`id`, `nome`) VALUES (1, 'Aventura');
+
+INSERT INTO generos(`id`, `nome`) VALUES (2, 'Terror'), (3, 'Suspense');
+
+INSERT INTO generos(`nome`) VALUES ('Policial');
+
+SELECT * FROM generos;
+
+INSERT INTO livros(titulo, id_genero) VALUES ('Assassinato no Expresso do Oriente', 4), ('A Sociedade do Anel', 1);
+
+SELECT
